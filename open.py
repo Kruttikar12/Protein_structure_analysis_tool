8a1fd76a41246cea6d4bc281e778cfc7dad47d4a
Kruttikar12 <kruttika1230@gmail.com>
2025-08-14 19:41:08 +0530

    Removed unwanted PDB and Python files

--- open.py
+++
@@ -1,3 +0,0 @@
-with open("C:/Users/hp/Downloads/1a3n.pdb", "r") as pdb_file:
-    for line in pdb_file:
-        print(line.strip())
--- print_SEQRES.py
+++
@@ -1,7 +0,0 @@
-with open("C:/Users/hp/Downloads/1a3n.pdb", "r") as pdb_file:
-    for line in pdb_file:
-        if line.startswith("SEQRES"):
-            chain_id=line[11].strip()
-            residue_num=line[13:17].strip()
-            residue_name=line[19:70]
-            print(f" Chain: {chain_id}, Residue No: {residue_num}, Residue: {residue_name}")
